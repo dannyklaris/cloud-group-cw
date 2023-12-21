ec42fe3f2fd32728de2cb3aa4e23b92be4ed90ba
daka1g21 <daka1g21@soton.ac.uk>
2023-12-21 22:18:37 +0000

    - completed the player_register function - updated the local.settings.json to include necessary config but with empty input

--- backend/player_register/__init__.py
+++ backend/player_register/__init__.py
@@ -1,24 +1,38 @@
 import logging
+import json
 
-import azure.functions as func
+from azure.functions import HttpRequest, HttpResponse
 
+import os
+from azure.cosmos import CosmosClient
+from azure.cosmos.exceptions import CosmosHttpResponseError, CosmosResourceExistsError, CosmosResourceNotFoundError
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+MyCosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
+DBProxy = MyCosmos.get_database_client(os.environ['Database'])
+PlayerProxy = DBProxy.get_container_client(os.environ['PlayerContainer'])
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+def main(req: HttpRequest) -> HttpResponse:
+    
+    req_body = req.get_json()
+    logging.info('Registering new player: {}'.format(req_body))
+    
+    new_player = { "username" : req_body['username'], "password" : req_body['password']}
+    
+    # check if player exists
+    query = f'SELECT * FROM c WHERE c.username = "{new_player["username"]}"'
+    existing_players = list(PlayerProxy.query_items(query, enable_cross_partition_query=True))
+    logging.info('Existing players: {}'.format(existing_players))
+    if existing_players:
+        return HttpResponse(body=json.dumps({"result": False, "msg" : "Username already exists"}), mimetype="application/json")
+    
+    # username too long or too short
+    if len(new_player["username"]) < 4 or len(new_player["username"]) > 14:
+        return HttpResponse(body=json.dumps({"result": False, "msg" : "Username less than 4 characters or more than 14 characters"}), mimetype="application/json")
+    
+    # password too long or too short
+    if len(new_player["password"]) < 6 or len(new_player["password"]) > 20:
+        return HttpResponse(body=json.dumps({"result": False, "msg" : "Password less than 6 characters or more than 20 characters"}), mimetype="application/json")
+    
+    # valid player
+    PlayerProxy.create_item(new_player, enable_automatic_id_generation=True)
+    return HttpResponse(body=json.dumps({"result": True, "msg" : "OK"}), mimetype="application/json")
