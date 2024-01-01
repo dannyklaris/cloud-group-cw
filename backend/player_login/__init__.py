21b4a1381a1050640abf963903fa6d6195123658
simran-ss-sandhu <simran.ss.sandhu23@gmail.com>
2024-01-01 21:41:57 +0000

    refactor(api): improve readability of 'player_login' function

--- backend/player_login/__init__.py
+++ backend/player_login/__init__.py
@@ -1,38 +1,37 @@
 import logging
 import json
-from azure.functions import HttpRequest,HttpResponse
 import os
+from azure.functions import HttpRequest,HttpResponse
 from azure.cosmos import CosmosClient
+
+
 cosmos = CosmosClient.from_connection_string(os.environ['AzureCosmosDBConnectionString'])
 database = cosmos.get_database_client(os.environ['Database'])
 container = database.get_container_client(os.environ['PlayerContainer'])
 
 
-
 def main(req: HttpRequest) -> HttpResponse:
-        loginPlayer = req.get_json()
-        username = loginPlayer.get('username')
-        password = loginPlayer.get('password')
-        
-        logging.info('login player by parameter {}'.format(loginPlayer))
-        
-        playerlist = f"SELECT * FROM c WHERE c.username = '{username}'"
-        usernamelist = list(container.query_items(playerlist, enable_cross_partition_query=True))
-        
-        # If usernamelist is empty, it means that the entered username does not exist
-        if not usernamelist:
-            return HttpResponse(
-                body=json.dumps({"result": False, "msg": "Username or password incorrect"}),mimetype="application/json"
-            )
-            
-        # There will only be one person in the usernamelist, so there will only be one password. If the password does not match, it will prove that the login failed.
-        elif usernamelist[0]['password'] != password:
-            return HttpResponse(
-                body=json.dumps({"result": False, "msg": "Username or password incorrect"}),mimetype="application/json"
-            )
-        
-        # If both username and password are correct, then ok.
-        else:
-            return HttpResponse(
-                body=json.dumps({"result": True, "msg": "OK"}),mimetype="application/json"
-            )
+    """Determine if player login details in request are valid"""
+
+    # Log request
+    loginRequest = req.get_json()
+    logging.info(f'Player login request = {loginRequest}')
+
+    # Extract username and password from request
+    username = loginRequest.get('username')
+    password = loginRequest.get('password')
+    
+    # Search for players that match request parameters
+    playerSelectQuery = f"SELECT c.username FROM c WHERE c.username = '{username}' and c.password = '{password}'"
+    matchingPlayers = list(container.query_items(playerSelectQuery, enable_cross_partition_query=True))
+    
+    if not matchingPlayers:
+        # Query result is empty - the login details in the request are invalid
+        return HttpResponse(
+            body=json.dumps({"result": False, "msg": "Username or password incorrect"}),mimetype="application/json"
+        )
+    else:
+        # A player exists that matches the login details in the request
+        return HttpResponse(
+            body=json.dumps({"result": True, "msg": "OK"}),mimetype="application/json"
+        )
