b4acb76cb0bb736994ce2b28bc8e1a0ff9cb5966
simran-ss-sandhu <simran.ss.sandhu23@gmail.com>
2024-01-02 16:29:03 +0000

    refactor(question generation): separate addition generation from main into a separate function

--- backend/question_get/__init__.py
+++ backend/question_get/__init__.py
@@ -8,6 +8,40 @@ import azure.functions as func
 NUM_OF_QUESTIONS = 10
 NUM_OF_ANSWERS = 4
 
+
+def newAdditionQuestion():
+    """Generate and return a single addition question"""
+
+    # generate numbers to use in question
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+
+    # add correct answer as one of the question answers
+    correctAnswer = num1 + num2
+    answers = [correctAnswer]
+
+    # generate three random other answers
+    while len(answers) < NUM_OF_ANSWERS:
+        randomAnswer = random.randint(1, 20)
+        if (randomAnswer not in answers):
+            answers.append(randomAnswer)
+    
+    # ensure correct answer is not always the first answer
+    random.shuffle(answers)
+
+    # create question
+    questionText = f'{num1} + {num2} = ?'
+    question = {
+        'question': questionText,
+        'answers': answers,
+        'correctAnswer': correctAnswer,
+        'difficulty': 'easy',
+        'topic': 'addition'
+    }
+
+    return question
+
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     """Generate questions for a user"""
 
@@ -15,35 +49,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     questionRequest = req.get_body()
     logging.info(f'Question get request = {questionRequest}')
 
+    # generate questions
     questions = []
     for _ in range(NUM_OF_QUESTIONS):
-        num1 = random.randint(1, 10)
-        num2 = random.randint(1, 10)
+        questions.append(newAdditionQuestion())
 
-        # add correct answer as one of the question answers
-        correctAnswer = num1 + num2
-        answers = [correctAnswer]
-
-        # generate three random other answers
-        while len(answers) < NUM_OF_ANSWERS:
-            randomAnswer = random.randint(1, 20)
-            if (randomAnswer not in answers):
-                answers.append(randomAnswer)
-
-        # correct answer is not always the first answer
-        random.shuffle(answers)
-
-        # create question
-        questionText = f'{num1} + {num2} = ?'
-        question = {
-            'question': questionText,
-            'answers': answers,
-            'correctAnswer': correctAnswer,
-            'difficulty': 'easy',
-            'topic': 'addition'
-        }
-        questions.append(question)
-
+    # return questions
     return func.HttpResponse(
         body=json.dumps(questions),mimetype="application/json"
     )
